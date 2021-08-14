f174b953f3e3790c97c8e337b36b409f544c7f31
anil305rgpv <36809011+anil503rgpv@users.noreply.github.com>
2021-08-14 12:51:03 +0000

    integer and float division

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-age = 30
+age = 30 #integer
 print(age)
 
-age = 40
+age = 40 
 print(age)
 
-PI = 3.14159
+PI = 3.14159 #float
+
+float_division = 12 / 3
+print (float_division)
+
+integer_division = 12 // 3
+print (integer_division)
